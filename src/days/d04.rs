5e208bfa928412160469ec9b499974ad6b1af53b
LeMoonStar <webmaster@unitcore.de>
2023-12-04 07:55:52 +0100

    Solved day 4 part 1

--- src/days/d04.rs
+++ src/days/d04.rs
@@ -2,14 +2,37 @@ use super::{Answer, Day, DayImpl};
 
 const CURRENT_DAY: u8 = 4;
 
-type Data = Vec<u64>;
+#[derive(Debug, Clone)]
+pub struct Card {
+    winning_numbers: Vec<u8>,
+    numbers: Vec<u8>,
+}
+
+impl From<&str> for Card {
+    fn from(value: &str) -> Self {
+        let (winning_list, numbers_list) = value.split_once(" | ").unwrap();
+
+        Self {
+            winning_numbers: winning_list
+                .split_whitespace()
+                .map(|v| v.parse().unwrap())
+                .collect(),
+            numbers: numbers_list
+                .split_whitespace()
+                .map(|v| v.parse().unwrap())
+                .collect(),
+        }
+    }
+}
+
+type Data = Vec<Card>;
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data) {
         Self::init(include_str!("test_inputs/test04.txt"))
     }
 
     fn expected_results() -> (Answer, Answer) {
-        (Answer::Number(0), Answer::Number(0))
+        (Answer::Number(13), Answer::Number(0))
     }
 
     fn init(input: &str) -> (Self, Data) {
@@ -17,13 +40,27 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
             Self {},
             input
                 .lines()
-                .map(|v| v.parse::<u64>().expect("error while parsing input."))
+                .map(|v| v.split_once(": ").unwrap().1.into())
                 .collect(),
         )
     }
 
     fn one(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let mut score = 0;
+
+        for card in data {
+            let mut card_score = 0;
+
+            for num in &card.numbers {
+                if card.winning_numbers.contains(num) {
+                    card_score = 1.max(card_score * 2);
+                }
+            }
+
+            score += card_score;
+        }
+
+        Answer::Number(score)
     }
 
     fn two(&self, data: &mut Data) -> Answer {
