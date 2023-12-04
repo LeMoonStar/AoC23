ec67ad715b0ccc21e03b2703fa88c7d50d61ad26
LeMoonStar <webmaster@unitcore.de>
2023-12-04 08:06:49 +0100

    Solved day 4 part 2

--- src/days/d04.rs
+++ src/days/d04.rs
@@ -4,15 +4,19 @@ const CURRENT_DAY: u8 = 4;
 
 #[derive(Debug, Clone)]
 pub struct Card {
+    card_id: u8,
     winning_numbers: Vec<u8>,
     numbers: Vec<u8>,
 }
 
 impl From<&str> for Card {
     fn from(value: &str) -> Self {
-        let (winning_list, numbers_list) = value.split_once(" | ").unwrap();
+        let (header, card) = value.split_once(": ").unwrap();
+
+        let (winning_list, numbers_list) = card.split_once(" | ").unwrap();
 
         Self {
+            card_id: header.split_whitespace().nth(1).unwrap().parse().unwrap(),
             winning_numbers: winning_list
                 .split_whitespace()
                 .map(|v| v.parse().unwrap())
@@ -32,17 +36,11 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     }
 
     fn expected_results() -> (Answer, Answer) {
-        (Answer::Number(13), Answer::Number(0))
+        (Answer::Number(13), Answer::Number(30))
     }
 
     fn init(input: &str) -> (Self, Data) {
-        (
-            Self {},
-            input
-                .lines()
-                .map(|v| v.split_once(": ").unwrap().1.into())
-                .collect(),
-        )
+        (Self {}, input.lines().map(|v| v.into()).collect())
     }
 
     fn one(&self, data: &mut Data) -> Answer {
@@ -64,6 +62,27 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     }
 
     fn two(&self, data: &mut Data) -> Answer {
+        let mut i = 0;
+
+        loop {
+            let mut wins = 0;
+
+            for num in &data[i].numbers {
+                if data[i].winning_numbers.contains(num) {
+                    wins += 1;
+                }
+            }
+
+            for j in data[i].card_id as usize..(data[i].card_id as usize + wins) {
+                data.push(data[j].clone());
+            }
+
+            i += 1;
+            if i >= data.len() {
+                break;
+            }
+        }
+
         Answer::Number(data.len() as u64)
     }
 }
