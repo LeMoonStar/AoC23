ca6d8106bcb8c8c2e9c45076b1d218a899efa987
LeMoonStar <webmaster@unitcore.de>
2023-12-06 10:40:11 +0100

    Refactored day 6

--- src/days/d06.rs
+++ src/days/d06.rs
@@ -1,24 +1,62 @@
+use num_traits::int::PrimInt;
+use std::ops::RangeBounds;
+use std::str::FromStr;
+
 use super::{Answer, Day, DayImpl};
 
 const CURRENT_DAY: u8 = 6;
 
 #[derive(Debug, Clone)]
-pub struct Race<T> {
+pub struct Race<T>
+where
+    T: PrimInt,
+{
     time: T,
     record: T,
 }
 
-impl Race<u16> {
+impl<T> Race<T>
+where
+    T: PrimInt,
+{
+    // This can be optimized by using maths - I intend on doing this soon,
+    // but I've been doing this day's task duwing my break at school and don't
+    // have enough time for this now.
+    fn check_race_range<R: RangeBounds<T> + IntoIterator<Item = T>>(
+        &self,
+        button_press_range: R,
+    ) -> u32 {
+        let mut wins = 0;
+
+        for button_press_time in button_press_range {
+            if self.check_race(button_press_time) {
+                wins += 1;
+            }
+        }
+
+        wins
+    }
+
+    fn check_race(&self, button_press_time: T) -> bool {
+        (self.time - button_press_time) * button_press_time > self.record
+    }
+}
+
+impl<T> Race<T>
+where
+    T: PrimInt + FromStr,
+    <T as FromStr>::Err: std::fmt::Debug,
+{
     fn parse_race_list(input: &str) -> Vec<Self> {
         let mut lines = input.lines();
-        let time_list: Vec<u16> = lines
+        let time_list: Vec<T> = lines
             .next()
             .unwrap()
             .split_whitespace()
             .skip(1)
             .map(|v| v.parse().unwrap())
             .collect();
-        let distacne_list: Vec<u16> = lines
+        let distance_list: Vec<T> = lines
             .next()
             .unwrap()
             .split_whitespace()
@@ -27,12 +65,9 @@ impl Race<u16> {
             .collect();
 
         time_list
-            .iter()
-            .zip(distacne_list.iter())
-            .map(|(time, distance)| Self {
-                time: *time,
-                record: *distance,
-            })
+            .into_iter()
+            .zip(distance_list)
+            .map(|(time, record)| Self { time, record })
             .collect()
     }
 }
@@ -54,19 +89,11 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn one(&self, data: &mut Data) -> Answer {
         let mut result = 1;
 
-        for race in &*data {
-            let mut wins = 0;
-            for button_press_time in 1..race.time {
-                let distance_travelled = (race.time - button_press_time) * button_press_time;
-                if distance_travelled > race.record {
-                    wins += 1;
-                }
-            }
-
-            result *= wins;
+        for race in data {
+            result *= race.check_race_range(1..race.time);
         }
 
-        Answer::Number(result)
+        Answer::Number(result as u64)
     }
 
     fn two(&self, data: &mut Data) -> Answer {
@@ -77,18 +104,7 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
                     + v.record as u64,
                 time: race.time * 10_u64.pow((v.time as f64).log(10.0) as u32 + 1) + v.time as u64,
             });
-        let mut result = 1;
 
-        let mut wins = 0;
-        for button_press_time in 1..race.time {
-            let distance_travelled = (race.time - button_press_time) * button_press_time;
-            if distance_travelled > race.record {
-                wins += 1;
-            }
-        }
-
-        result *= wins;
-
-        Answer::Number(result)
+        Answer::Number(race.check_race_range(1..race.time) as u64)
     }
 }
